65dd3ec2e729c7c4550015893ab50d9803b1716a
Jeremy DEVERDUN <jeremy.deverdun@neuf.fr>
2014-04-08 16:30:28 +0200

    ras

--- GdcServerManager/install/creationDB.sql
+++ GdcServerManager/install/creationDB.sql
@@ -57,7 +57,7 @@ constraint niftiimage_pk primary key (id),
 constraint uprot_serie_acqdata_patient_projet_namenifti unique(name,id_project,id_patient,id_acqdate,id_protocol,id_serie),
 constraint serienifti_fk foreign key (id_serie) references Serie(id) on delete cascade);
 
---RAJOUT TABLE JOBS
+--RAJOUT TABLE JOBS CONDOR
 create table job
 (id integer NOT NULL AUTO_INCREMENT, id_user integer, jobid varchar(10), submitDate date, os varchar(20), description varchar(300),
 constraint jobs_pk primary key(id),
