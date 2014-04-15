4e57bbcfb0e1b61e5f3035de6767ceca048d8e55
Moyano Joris <Moyano Joris@120.40.30.101>
2014-04-15 17:52:15 +0200

    pluging resting state

---
+++ GdcServerManager/src/test/TestFrame2.java
@@ -0,0 +1,16 @@
+package test;
+
+import javax.swing.JFrame;
+import javax.swing.JEditorPane;
+import javax.swing.UIManager;
+
+import org.pushingpixels.substance.api.SubstanceLookAndFeel;
+import org.pushingpixels.substance.api.skin.SubstanceGraphiteLookAndFeel;
+
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TestFrame2 extends JFrame {
+	
+}
