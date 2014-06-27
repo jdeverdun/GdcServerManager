54076ae50203df59af0d0e2e134af8283bed6962
Jeremy DEVERDUN <jeremy.deverdun@neuf.fr>
2014-06-27 16:54:17 +0200

    ras

--- GdcServerManager/lib/MATLAB/batch_restingState3.m
+++ GdcServerManager/lib/MATLAB/batch_restingState3.m
@@ -2,25 +2,31 @@ try
     mapdrive;
     folder = '#15#';
     t1reg = 'T1';
-    restreg = 'REST';
+    coudeDroit = 'coude_droit';
+	coudeGauche = 'coude_gauche';
+	coudeGD = 'coude_droit_+_gauche';
     grereg = 'gre_field';
     cd (folder);
     clear pinfo
-    pinfo.name = '#16#';
+    pinfo.name = '#19#';
     pinfo.dir = folder;
     date=#18#;
-    if(date==0)
-        pinfo.acquisition='#19#';
-    end
+    %if(date==0)
+        %pinfo.acquisition='#19#';
+    %end
     t1dir = dir(['*' t1reg '*']);
-    restdir = dir(['*' restreg '*']);
+    coudeDroitdir = dir(['*' coudeDroit '*']);
+	coudeGauchedir = dir(['*' coudeGauche '*']);
+	coudeGDdir = dir(['*' coudeGD '*']);
     gredir = dir(['*' grereg '*']);
     protocol=#20#;
     i=1;
     j=1;
     if(protocol==0)
         t1dir2 = dir([t1dir.name '/*' t1reg '*']);
-        restdir2 = dir([restdir.name '/*' restreg '*']);
+        coudeDroitdir2 = dir([coudeDroitdir.name '/*' coudeDroit '*']);
+		coudeGauchedir2 = dir([coudeGauchedir.name '/*' coudeGauche '*']);
+		coudeGDdir2 = dir([coudeGDdir.name '/*' coudeGD '*']);
         gredir2 = dir([gredir.name '/*' grereg '*']);
         while(i<=numel(t1dir2))
             if(isempty(regexpi(t1dir2(i).name, '.*T1.*3D.*GADO.*')))
@@ -31,28 +37,36 @@ try
             end
         end
         pinfo.t1.dir = [pinfo.dir '\' t1dir.name '\' t1dir2(j).name];
-        pinfo.rest.dir = [pinfo.dir '\' restdir.name '\' restdir(1).name];
-        pinfo.grefield.dir = [pinfo.dir '\' gredir.name '\' gredir(1).name];
+        pinfo.coudeDroit.dir = [pinfo.dir '\' coudeDroitdir.name '\' coudeDroitdir(1).name];
+		pinfo.coudeGauche.dir = [pinfo.dir '\' coudeGauchedir.name '\' coudeGauchedir.name];
+		pinfo.coudeGD.dir = [pinfo.dir '\' coudeGDdir.name '\' coudeGDdir.name];
+        pinfo.grefield.dir = char([pinfo.dir '\' gredir.name '\' gredir(1).name], [pinfo.dir '\' gredir.name '\' gredir(2).name]);
     else
         pinfo.t1.dir = [pinfo.dir '\' t1dir(1).name];
-        pinfo.rest.dir = [pinfo.dir '\' restdir(1).name];
-        pinfo.grefield.dir = [pinfo.dir '\' gredir(1).name];
+        pinfo.coudeDroit.dir = [pinfo.dir '\' coudeDroitdir(1).name];
+		pinfo.coudeGauche.dir = [pinfo.dir '\' coudeGauchedir.name];
+		pinfo.coudeGD.dir = [pinfo.dir '\' coudeGDdir.name];
+        pinfo.grefield.dir = char([pinfo.dir '\' gredir(1).name],[pinfo.dir '\' gredir(2).name]);
     end
     
-    pinfo.t1.files = spm_select('FPList',pinfo.t1.dir,['^' pinfo.name '.*.nii']);
-    pinfo.rest.files = spm_select('FPList',pinfo.rest.dir,['^' pinfo.name '.*.nii']);
+    pinfo.t1.files = spm_select('FPList',pinfo.t1.dir,['^' pinfo.name '.*.img']);
+    pinfo.coudeDroit.files = spm_select('FPList',pinfo.coudeDroit.dir,['^' pinfo.name '.*.img']);
+	pinfo.coudeGauche.files = spm_select('FPList',pinfo.coudeGauche.dir,['^' pinfo.name '.*.img']);
+	pinfo.coudeGD.files = spm_select('FPList',pinfo.coudeGD.dir,['^' pinfo.name '.*.img']);
 	RmImage=#21#;
 	if(RmImage==0)
-		pinfo.rest.files=pinfo.rest.files(#22#+1:end,:);
+		pinfo.coudeDroit.files=pinfo.coudeDroit.files(#22#+1:end,:);
+		pinfo.coudeGauche.files=pinfo.coudeGauche.files(#22#+1:end,:);
+		pinfo.coudeGD.files=pinfo.coudeGD.files(#22#+1:end,:);
 	end
     if(~(isempty(gredir)))
-        pinfo.grefield.files = spm_select('FPList',pinfo.grefield.dir,['^' pinfo.name '.*.nii']);
+        pinfo.grefield.files = char(spm_select('FPList',pinfo.grefield.dir(1,:),['^' pinfo.name '.*.img']), spm_select('FPList',pinfo.grefield.dir(2,:),['^' pinfo.name '.*.img']));
         if(isempty(pinfo.grefield.files))
             error(['Empty grefield files for ' pinfo.name]);
         end
-        pinfo.all.files= char(pinfo.t1.files, pinfo.rest.files, pinfo.grefield.files);
+        pinfo.all.files= char(pinfo.t1.files, pinfo.coudeDroit.files, pinfo.coudeGauche.files, pinfo.coudeGD.files, pinfo.grefield.files);
     else
-        pinfo.all.files= char(pinfo.t1.files, pinfo.rest.files);
+        pinfo.all.files= char(pinfo.t1.files, pinfo.coudeDroit.files, pinfo.coudeGauche.files, pinfo.coudeGD.files);
     end
     if(isempty(pinfo.t1.files))
         error(['Empty t1 files for ' pinfo.name]);
@@ -77,26 +91,6 @@ try
 		end
         trouve=false;
         i=1;
-        if(date==0)
-            while(i<=ligne)
-                if((isequal(num2str(raw{i,1}),pinfo.name) || isequal(raw{i,1},pinfo.name)) && isequal(num2str(raw{i,2}),pinfo.acquisition))
-                    trouve=true;
-                    break;
-                else
-                    i=i+1;
-                end
-            end
-            if(trouve)
-                matlabbatch{1}.spm.util.reorient.transform.transprm = [-(str2num(raw{i,3})) -(str2num(raw{i,4})) -(str2num(raw{i,5})) 0 0 0 1 1 1 0 0 0];
-                if(isequal(class(raw{i,1}),'double'))
-                    fprintf(['ligne : ' num2str(i) ', name : ' num2str(raw{i,1}) ', date : ' num2str(raw{i,2}) ', x : ' num2str(raw{i,3}) ', y : ' num2str(raw{i,4}) ', z : ' num2str(raw{i,5})]);
-                else
-                    fprintf(['ligne : ' num2str(i) ', name : ' raw{i,1} ', date : ' num2str(raw{i,2}) ', x : ' num2str(raw{i,3}) ', y : ' num2str(raw{i,4}) ', z : ' num2str(raw{i,5})]);
-                end
-            else
-                error(['Patient ' pinfo.name ' : not find in the xls file at the date ' pinfo.acquisition]);
-            end
-        else
             while(i<=ligne)
                 if(isequal(num2str(raw{i,1}),pinfo.name) || isequal(raw{i,1},pinfo.name))
                     trouve=true;
@@ -106,7 +100,7 @@ try
                 end
             end
             if(trouve)
-                matlabbatch{1}.spm.util.reorient.transform.transprm = [-(str2num(raw{i,2})) -(str2num(raw{i,3})) -(str2num(raw{i,4})) 0 0 0 1 1 1 0 0 0];
+                matlabbatch{1}.spm.util.reorient.transform.transprm = [-(raw{i,2}) -(raw{i,3}) -(raw{i,4}) 0 0 0 1 1 1 0 0 0];
                 if(isequal(class(raw{i,1}),'double'))
                     fprintf(['ligne : ' num2str(i) ', name : ' num2str(raw{i,1}) ', x : ' num2str(raw{i,2}) ', y : ' num2str(raw{i,3}) ', z : ' num2str(raw{i,4})]);
                 else
@@ -115,17 +109,20 @@ try
             else
                 error(['Patient ' pinfo.name ' : not find in the xls file']);
             end
-        end
         matlabbatch{1}.spm.util.reorient.prefix = 'o_';
         spm_jobman('run',matlabbatch);
         pinfo.t1.files = spm_select('FPList',pinfo.t1.dir,['^o_' pinfo.name '.*.nii']);
-        pinfo.rest.files = spm_select('FPList',pinfo.rest.dir,['^o_' pinfo.name '.*.nii']);
+        pinfo.coudeDroit.files = spm_select('FPList',pinfo.coudeDroit.dir,['^o_' pinfo.name '.*.nii']);
+		pinfo.coudeGauche.files = spm_select('FPList',pinfo.coudeGauche.dir,['^o_' pinfo.name '.*.nii']);
+		pinfo.coudeGD.files = spm_select('FPList',pinfo.coudeGD.dir,['^o_' pinfo.name '.*.nii']);
         pinfo.grefield.files = spm_select('FPList',pinfo.grefield.dir,['^o_' pinfo.name '.*.nii']);
     end
     
     if(reorientationFlag==2)
         pinfo.t1.files = spm_select('FPList',pinfo.t1.dir,['^o_' pinfo.name '.*.nii']);
-        pinfo.rest.files = spm_select('FPList',pinfo.rest.dir,['^o_' pinfo.name '.*.nii']);
+        pinfo.coudeDroit.files = spm_select('FPList',pinfo.coudeDroit.dir,['^o_' pinfo.name '.*.nii']);
+		pinfo.coudeGauche.files = spm_select('FPList',pinfo.coudeGauche.dir,['^o_' pinfo.name '.*.nii']);
+		pinfo.coudeGD.files = spm_select('FPList',pinfo.coudeGD.dir,['^o_' pinfo.name '.*.nii']);
         pinfo.grefield.files = spm_select('FPList',pinfo.grefield.dir,['^o_' pinfo.name '.*.nii']);
     end
     
@@ -168,9 +165,9 @@ try
             error(['Empty grefield.vdm files for ' pinfo.name]);
         end
     end
-    %% slice timing resting state
+    %% slice timing
     clear matlabbatch
-    matlabbatch{1}.spm.temporal.st.scans = {cellstr(pinfo.rest.files)};
+    matlabbatch{1}.spm.temporal.st.scans = {cellstr(pinfo.coudeGauche.files)};
     matlabbatch{1}.spm.temporal.st.nslices = #5#;%utilisateur
     matlabbatch{1}.spm.temporal.st.tr = #6#;%utilisateur
     matlabbatch{1}.spm.temporal.st.ta = #7#;%utilisateur
@@ -178,20 +175,19 @@ try
     matlabbatch{1}.spm.temporal.st.refslice = #9#;%utilisateur
     matlabbatch{1}.spm.temporal.st.prefix = 'a';
     spm_jobman('run',matlabbatch);
-    if(reorientationFlag==0 || reorientationFlag==2)
-        pinfo.rest.st.files = spm_select('FPList',pinfo.rest.dir,['^ao_' pinfo.name '.*.nii']);
-		
+   if(reorientationFlag==0 || reorientationFlag==2)
+        pinfo.coudeGauche.st.files = spm_select('FPList',pinfo.coudeGauche.dir,['^ao_' pinfo.name '.*.img']);
     else
-        pinfo.rest.st.files = spm_select('FPList',pinfo.rest.dir,['^a' pinfo.name '.*.nii']);
+        pinfo.coudeGauche.st.files = spm_select('FPList',pinfo.coudeGauche.dir,['^a' pinfo.name '.*.img']);
     end
-    if(isempty(pinfo.rest.st.files))
-        error(['Empty rest.st.files files for ' pinfo.name]);
+    if(isempty(pinfo.coudeGauche.st.files))
+        error(['Empty coudeGauche.st.files files for ' pinfo.name]);
     end
     
-    %% realignement du resting state avec grefield
+    %% realignement avec grefield
     if(presubtractFlag==0 || presubtractFlag==2)
         clear matlabbatch
-        matlabbatch{1}.spm.spatial.realignunwarp.data.scans = cellstr(pinfo.rest.st.files);
+		matlabbatch{1}.spm.spatial.realignunwarp.data.scans = cellstr(pinfo.coudeGauche.st.files);
         matlabbatch{1}.spm.spatial.realignunwarp.data.pmscan = cellstr(pinfo.grefield.vdm5.files);
         matlabbatch{1}.spm.spatial.realignunwarp.eoptions.quality = 0.9;
         matlabbatch{1}.spm.spatial.realignunwarp.eoptions.sep = 4;
@@ -218,24 +214,24 @@ try
         spm_jobman('run',matlabbatch);
         
         if(reorientationFlag==0 || reorientationFlag==2)
-            pinfo.rest.realign.files = spm_select('FPList',pinfo.rest.dir,['^uao_' pinfo.name '.*.nii']);
-            pinfo.rest.realign.mean.files = spm_select('FPList',pinfo.rest.dir,['^meanuao_' pinfo.name '.*.nii']);
+            pinfo.coudeGauche.realign.files = spm_select('FPList',pinfo.coudeGauche.dir,['^uao_' pinfo.name '.*.img']);
+            pinfo.coudeGauche.realign.mean.files = spm_select('FPList',pinfo.coudeGauche.dir,['^meanuao_' pinfo.name '.*.img']);
         else
-            pinfo.rest.realign.files = spm_select('FPList',pinfo.rest.dir,['^ua' pinfo.name '.*.nii']);
-            pinfo.rest.realign.mean.files = spm_select('FPList',pinfo.rest.dir,['^meanua' pinfo.name '.*.nii']);
+            pinfo.coudeGauche.realign.files = spm_select('FPList',pinfo.coudeGauche.dir,['^ua' pinfo.name '.*.img']);
+            pinfo.coudeGauche.realign.mean.files = spm_select('FPList',pinfo.coudeGauche.dir,['^meanua' pinfo.name '.*.img']);
         end
         
-        if(isempty(pinfo.rest.realign.files))
-            error(['Empty rest.realign.files files for ' pinfo.name]);
+        if(isempty(pinfo.coudeGauche.realign.files))
+            error(['Empty coudeGauche.realign.files files for ' pinfo.name]);
         end
-        
-        if(isempty(pinfo.rest.realign.mean.files))
-            error(['Empty rest.realign.mean.files files for ' pinfo.name]);
+
+        if(isempty(pinfo.coudeGauche.realign.mean.files))
+            error(['Empty coudeGauche.realign.mean.files files for ' pinfo.name]);
         end
     else
-        % realignement du resting state sans grefield
-        matlabbatch{1}.spm.spatial.realign.estwrite.data = cellstr(pinfo.rest.st.files);
-        matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
+        % realignement sans grefield
+        matlabbatch{1}.spm.spatial.realignunwarp.data.scans = cellstr(pinfo.coudeGauche.st.files);
+        matlabbatch{1}.spm.spatial.realignunwarp.data.pmscan = cellstr(pinfo.grefield.vdm5.files);
         matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.sep = 4;
         matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
         matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
@@ -249,26 +245,25 @@ try
         matlabbatch{1}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
         spm_jobman('run',matlabbatch);
         if(reorientationFlag==0 || reorientationFlag==2)
-            pinfo.rest.realign.files = spm_select('FPList',pinfo.rest.dir,['^rao_' pinfo.name '.*.nii']);
-            pinfo.rest.realign.mean.files = spm_select('FPList',pinfo.rest.dir,['^meanrao_' pinfo.name '.*.nii']);
+            pinfo.coudeGauche.realign.files = spm_select('FPList',pinfo.coudeGauche.dir,['^rao_' pinfo.name '.*.img']);
+            pinfo.coudeGauche.realign.mean.files = spm_select('FPList',pinfo.coudeGauche.dir,['^meanrao_' pinfo.name '.*.img']);
         else
-            pinfo.rest.realign.files = spm_select('FPList',pinfo.rest.dir,['^ra' pinfo.name '.*.nii']);
-            pinfo.rest.realign.mean.files = spm_select('FPList',pinfo.rest.dir,['^meanra' pinfo.name '.*.nii']);
+            pinfo.coudeGauche.realign.files = spm_select('FPList',pinfo.coudeGauche.dir,['^ra' pinfo.name '.*.img']);
+            pinfo.coudeGauche.realign.mean.files = spm_select('FPList',pinfo.coudeGauche.dir,['^meanra' pinfo.name '.*.img']);
         end
-        
-        if(isempty(pinfo.rest.realign.files))
-            error(['Empty rest.realign.files files for ' pinfo.name]);
+		
+        if(isempty(pinfo.coudeGauche.realign.files))
+            error(['Empty coudeGauche.realign.files files for ' pinfo.name]);
         end
-        
-        if(isempty(pinfo.rest.realign.mean.files))
-            error(['Empty rest.realign.mean.files files for ' pinfo.name]);
+        if(isempty(pinfo.coudeGauche.realign.mean.files))
+            error(['Empty coudeGauche.realign.mean.files files for ' pinfo.name]);
         end
     end
-    %% coregistration du resting state
+    %% coregistration
     clear matlabbatch
     matlabbatch{1}.spm.spatial.coreg.estimate.ref = cellstr(pinfo.t1.files);
-    matlabbatch{1}.spm.spatial.coreg.estimate.source = cellstr(pinfo.rest.realign.mean.files);
-    matlabbatch{1}.spm.spatial.coreg.estimate.other = cellstr(pinfo.rest.realign.files);
+     matlabbatch{1}.spm.spatial.coreg.estimate.source = cellstr(pinfo.coudeGauche.realign.mean.files);
+    matlabbatch{1}.spm.spatial.coreg.estimate.other = cellstr(pinfo.coudeGauche.realign.files);
     matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
     matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
     matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
@@ -310,12 +305,12 @@ try
 		pinfo.t1.segment.wm = spm_select('FPList',pinfo.t1.dir,['^c2' pinfo.name '.*.nii']);
 		pinfo.t1.segment.csf = spm_select('FPList',pinfo.t1.dir,['^c3' pinfo.name '.*.nii']);
     end
-    %% Normalisation du Resting-state
+    %% Normalisation
     template=#14#;
     clear matlabbatch
     matlabbatch{1}.spm.spatial.normalise.estwrite.subj.source = cellstr(pinfo.t1.files);
     matlabbatch{1}.spm.spatial.normalise.estwrite.subj.wtsrc = '';
-	temp = cellstr(pinfo.rest.realign.files);
+	temp = cellstr(pinfo.coudeGauche.realign.files);
 	temp{end+1} = pinfo.t1.files;
 	temp{end+1} = pinfo.t1.segment.gm;
 	temp{end+1} = pinfo.t1.segment.wm;
@@ -343,34 +338,34 @@ try
     matlabbatch{1}.spm.spatial.normalise.estwrite.roptions.prefix = 'w';
     spm_jobman('run',matlabbatch);
     if(reorientationFlag==0 || reorientationFlag==2)
-        pinfo.rest.norm.files = spm_select('FPList',pinfo.rest.dir,['^wuao_' pinfo.name '.*.nii']);
+        pinfo.coudeGauche.norm.files = spm_select('FPList',pinfo.coudeGauche.dir,['^wuao_' pinfo.name '.*.img']);
     else
-        pinfo.rest.norm.files = spm_select('FPList',pinfo.rest.dir,['^wua' pinfo.name '.*.nii']);
+        pinfo.coudeGauche.norm.files = spm_select('FPList',pinfo.coudeGauche.dir,['^wua' pinfo.name '.*.img']);
     end
     
-    if(isempty(pinfo.rest.norm.files))
-        error(['Empty rest.norm.files files for ' pinfo.name]);
+    if(isempty(pinfo.coudeGauche.norm.files))
+        error(['Empty coudeGauche.norm.files files for ' pinfo.name]);
     end
     
-    %% Smooth Resting-state
+    %% Smooth
     clear matlabbatch
-    matlabbatch{1}.spm.spatial.smooth.data = cellstr(pinfo.rest.norm.files);
+    matlabbatch{1}.spm.spatial.smooth.data = cellstr(pinfo.coudeGauche.norm.files);
     matlabbatch{1}.spm.spatial.smooth.fwhm = [#10#]; %utilisateur
     matlabbatch{1}.spm.spatial.smooth.dtype = 0;
     matlabbatch{1}.spm.spatial.smooth.im = 0;
     matlabbatch{1}.spm.spatial.smooth.prefix = 's';
     spm_jobman('run',matlabbatch);
     if(reorientationFlag==0 || reorientationFlag==2)
-        pinfo.rest.smoothnorm.files = spm_select('FPList',pinfo.rest.dir,['^swuao_' pinfo.name '.*.nii']);
+        pinfo.coudeGauche.smoothnorm.files = spm_select('FPList',pinfo.coudeGauche.dir,['^swuao_' pinfo.name '.*.img']);
     else
-        pinfo.rest.smoothnorm.files = spm_select('FPList',pinfo.rest.dir,['^swua' pinfo.name '.*.nii']);
+        pinfo.coudeGauche.smoothnorm.files = spm_select('FPList',pinfo.coudeGauche.dir,['^swua' pinfo.name '.*.img']);
     end
     
-    if(isempty(pinfo.rest.smoothnorm.files))
-        error(['Empty rest.smoothnorm.files files for ' pinfo.name]);
+    if(isempty(pinfo.coudeGauche.smoothnorm.files))
+        error(['Empty coudeGauche.smoothnorm.files files for ' pinfo.name]);
     end
     
-    save([pinfo.dir '/pinfo.mat'],'pinfo');
+    save([pinfo.dir '/pinfo2.mat'],'pinfo');
 catch exception
     disp(exception.message);
     quit;
